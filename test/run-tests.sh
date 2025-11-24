9ff9edb45fc68d4054880e22b78ef30e089f52af
Eachan Johnson <eachan.johnson@crick.ac.uk>
2025-11-24 11:29:37 +0000

    Fix tests

---
+++ test/run-tests.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -exuo pipefail
+
+MODE=${1:-no}
+script_dir="$(readlink -f $(dirname $0))"
+start_dir="$(pwd)"
+
+if [ "$MODE" == "gh" ]
+then
+    export NXF_CONTAINER_ENGINE=docker
+    # export NXF_DOCKER_OPTS="-u $(id -u):$(id -g)"
+    docker_flag='-profile gh'
+elif [ "$MODE" == "crick" ]
+then 
+    export SINGULARITY_FAKEROOT=1
+    export NXF_CONTAINER_ENGINE=singularity
+    docker_flag='-profile standard'
+    module load Singularity Nextflow
+else
+    docker_flag=''
+fi
+
+# Examples with sample sheet
+nextflow run "$script_dir"/.. \
+    -resume $docker_flag \
+    -work-dir "$script_dir"/work
